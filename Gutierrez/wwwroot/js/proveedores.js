941565dc533a39271934336b3df5e383e1a5a1b1
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-11-21 17:24:40 -0600

    .

--- Gutierrez/wwwroot/js/proveedores.js
+++ Gutierrez/wwwroot/js/proveedores.js
@@ -1,48 +1,8 @@
-﻿// async function DescargarDatos() {
-//     try {
-//         // Realizar la solicitud a la API
-//         const response = await fetch(`${ApiUrl_local}Proveedor/proveedores`);
-
-//         // Verificar si la respuesta es exitosa
-//         if (response.ok) {
-//             // Convertir la respuesta a JSON
-//             const proveedores = await response.json();
-
-//             // Llamar a una función para actualizar la tabla con los datos
-//             actualizarTabla(proveedores);
-//         } else {
-//             console.error("Error al obtener los datos:", response.statusText);
-//         }
-//     } catch (error) {
-//         // Manejo de errores
-//         console.error("Hubo un problema con la solicitud fetch:", error);
-//     }
-// }
-// // Función para actualizar dinámicamente la tabla con los datos recibidos
-// function actualizarTabla(proveedores) {
-//     const tbody = document.querySelector("tbody"); // Seleccionar el cuerpo de la tabla
-//     // Limpiar contenido previo del tbody
-//     tbody.innerHTML = "";
-//     // Iterar sobre los proveedores y crear filas para la tabla
-//     proveedores.forEach(proveedor => {
-//         const row = document.createElement("tr");
-//         row.innerHTML = `
-//             <td>${proveedor.id}</td>
-//             <td><a id="DetalleProveedor" class="link">${proveedor.nombre}</a></td>
-//             <td><span class="repse-indicator-${proveedor.indicador}"></span></td>
-//             <td>${proveedor.fecha}</td>
-//         `;
-//         tbody.appendChild(row);
-//     });
-// }
-// // Llamar a la función para obtener los datos y mostrar en la tabla
-// DescargarDatos();
-
-async function obtenerProveedores() {
+﻿async function obtenerProveedores() {
     //LLAMAR A LA API CON FETCH
     try {
         //const response = await fetch(`${ApiUrl_local}Proveedor/proveedores`);
-        const response = await fetch(ApiUrl_local+'proveedor'); // Cambia la URL según tu entorno
+        const response = await fetch(ApiUrl_local+'proveedor'); 
         if (response.ok) {
             const proveedores = await response.json(); // Convertir la respuesta en JSON
             mostrarProveedores(proveedores); // Llamar a la función para mostrar los datos
