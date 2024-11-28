6c6b0caa28644fc8eabfd071f5d9c98338cd359c
LuisRiveraGzz563 <luisriverapto@outlook.com>
2024-11-27 23:58:44 -0600

    .

--- Gutierrez/wwwroot/js/Documento.js
+++ Gutierrez/wwwroot/js/Documento.js
@@ -2,29 +2,28 @@
     const tableBody = document.querySelector(".data-table-p tbody");
 
     // Función para llenar la tabla
-    const fillTable = async () => {
+    async function fillTable()
+    {
         try {
-            const response = await fetch(ApiUrlRelease + 'api/Documentos');
-            console.log(await response.json());
+            const response = await fetch(ApiUrlRelease + 'api/Proveedor/Solicitudes');
             if (!response.ok) {
                 throw new Error("Error al obtener los datos de la API");
             }
             else {
                 console.log("entro");
             }
-
             const proveedores = await response.json();
 
+            console.log(proveedores);
             // Limpiar el contenido existente de la tabla
             tableBody.innerHTML = "";
 
             // Renderizar los datos en la tabla
             proveedores.forEach((proveedor, index) => {
                 const row = document.createElement("tr");
-
                 row.innerHTML = `
-                <td>${proveedor.proveedorDocumento.rfc}</td>
-                <td>${proveedor.proveedorDocumento.nombre}</td>
+                <td>${proveedor.rfc}</td>
+                <td>${proveedor.nombre}</td>
                 <td>
                     <input type="radio" name="status_${index}" id="status_${index}_1" value="1" onclick="toggleStatus(1, ${index})" />
                     <input type="radio" name="status_${index}" id="status_${index}_0" value="0" onclick="toggleStatus(0, ${index})" />
