77a59a53653f3fa642a25f55f979299540eea02c
LuisRiveraGzz563 <luisriverapto@outlook.com>
2024-11-28 01:00:19 -0600

    repse proveedor palomita funcionando

--- Gutierrez/wwwroot/js/Documento.js
+++ Gutierrez/wwwroot/js/Documento.js
@@ -13,11 +13,8 @@
                 console.log("entro");
             }
             const proveedores = await response.json();
-
-            console.log(proveedores);
             // Limpiar el contenido existente de la tabla
             tableBody.innerHTML = "";
-
             // Renderizar los datos en la tabla
             proveedores.forEach((proveedor, index) => {
                 const row = document.createElement("tr");
@@ -25,16 +22,21 @@
                 <td>${proveedor.rfc}</td>
                 <td>${proveedor.nombre}</td>
                 <td>
-                    <input type="radio" name="status_${index}" id="status_${index}_1" value="1" onclick="toggleStatus(1, ${index})" />
-                    <input type="radio" name="status_${index}" id="status_${index}_0" value="0" onclick="toggleStatus(0, ${index})" />
-                    <label class="status-label" for="status_${index}_1">✘</label>
+                    <input type="radio" name="status_${proveedor.estado}" id="status_${proveedor.estado}_1" value="1" onclick="toggleStatus(1, ${proveedor.estado})" />
+                    <input type="radio" name="status_${proveedor.estado}" id="status_${proveedor.estado}_0" value="0" onclick="toggleStatus(0, ${proveedor.estado})" />
+                    <label class="status-label" for="status_${proveedor.estado}_1">✘</label>
                 </td>
                 <td>
                     <button class="accept-button">Aceptar</button>
-                    <button class="reject-button">Rechazar</button>
-                </td>
+                    <button class="reject-button">Rechazar</button>                                                               
+                </td>                                                               
                     `;
-                tableBody.appendChild(row);
+                let clon = row.cloneNode(true);
+                if (proveedor.estado == 1) {
+                    clon.querySelector('.status-label').innerHTML = '✔';
+                    clon.querySelector('.status-label').style.color = "forestGreen";
+                }
+                tableBody.appendChild(clon);
             });
         } catch (error) {
             console.error("Error al llenar la tabla:", error);
