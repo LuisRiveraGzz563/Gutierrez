7313845f093e3162ea8733d37a59efa113b45b3e
LuisRiveraGzz563 <luisriverapto@outlook.com>
2024-11-28 01:59:21 -0600

    Filtro de UsuariosView funcionando

--- Gutierrez/wwwroot/js/Documento.js
+++ Gutierrez/wwwroot/js/Documento.js
@@ -1,24 +1,22 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    const tableBody = document.querySelector(".data-table-p tbody");
+﻿let ListaProveedores = [];
+const tableBody = document.querySelector(".data-table-p tbody");
+async function filtro() {
+    let nombre = document.getElementById('lblnombre').value;
+    let rfc = document.getElementById('lblrfc').value;
+    let proveedores = ListaProveedores.filter(proveedor =>
+        proveedor.nombre.toLowerCase().includes(nombre.toLowerCase())
+        && proveedor.rfc.toLowerCase().includes(rfc.toLowerCase())
+    );
+    llenarTabla(proveedores);
+}
 
-    // Función para llenar la tabla
-    async function fillTable()
-    {
-        try {
-            const response = await fetch(ApiUrlRelease + 'api/Proveedor/Solicitudes');
-            if (!response.ok) {
-                throw new Error("Error al obtener los datos de la API");
-            }
-            else {
-                console.log("entro");
-            }
-            const proveedores = await response.json();
-            // Limpiar el contenido existente de la tabla
-            tableBody.innerHTML = "";
-            // Renderizar los datos en la tabla
-            proveedores.forEach((proveedor, index) => {
-                const row = document.createElement("tr");
-                row.innerHTML = `
+async function llenarTabla(proveedores) {
+    // Limpiar el contenido existente de la tabla
+    tableBody.innerHTML = "";
+    // Renderizar los datos en la tabla
+    proveedores.forEach((proveedor, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
                 <td>${proveedor.rfc}</td>
                 <td>${proveedor.nombre}</td>
                 <td>
@@ -31,16 +29,29 @@
                     <button class="reject-button">Rechazar</button>                                                               
                 </td>                                                               
                     `;
-                let clon = row.cloneNode(true);
-                if (proveedor.estado == 1) {
-                    clon.querySelector('.status-label').innerHTML = '✔';
-                    clon.querySelector('.status-label').style.color = "forestGreen";
-                }
-                tableBody.appendChild(clon);
-            });
-        } catch (error) {
-            console.error("Error al llenar la tabla:", error);
+        let clon = row.cloneNode(true);
+        if (proveedor.estado == 1) {
+            clon.querySelector('.status-label').innerHTML = '✔';
+            clon.querySelector('.status-label').style.color = "forestGreen";
         }
-    };
-    fillTable();
+        tableBody.appendChild(clon);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    try {
+        const response = await fetch(ApiUrlRelease + 'api/Proveedor/Solicitudes');
+        if (!response.ok) {
+            throw new Error("Error al obtener los datos de la API");
+        }
+        else {
+            console.log("entro");
+        }
+        const proveedores = await response.json();
+        ListaProveedores = proveedores;
+        llenarTabla(proveedores);
+    } catch (error) {
+        console.error("Error al llenar la tabla:", error);
+    }
 });
