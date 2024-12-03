03b77d0eaf9b8c0b268d56e8b1ae768db465725f
karime17 <karimeramos930@gmail.com>
2024-12-03 04:32:31 -0600

    .

---
+++ Gutierrez/Pages/Proveedor/DetalleProveedor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gutierrez.Pages.Proveedor
+{
+    public class DetalleProveedorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
