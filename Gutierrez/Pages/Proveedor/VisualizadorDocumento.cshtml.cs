80050aad8b68fca192c42d2524c9784a523dc61d
karime17 <karimeramos930@gmail.com>
2024-12-03 05:00:14 -0600

    Vista Visualizador en area proveedor

---
+++ Gutierrez/Pages/Proveedor/VisualizadorDocumento.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gutierrez.Pages.Proveedor
+{
+    public class VisualizadorDocumentoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
