0721ee66672598b304aefdcba696f543cca3edd1
karime17 <karimeramos930@gmail.com>
2024-12-03 00:53:25 -0600

    vista de agregar documento y css (actualizado)

---
+++ Gutierrez/Pages/Admin/Documento/AgregarDocumento.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gutierrez.Pages.Admin.Documento
+{
+    public class AgregarDocumentoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
